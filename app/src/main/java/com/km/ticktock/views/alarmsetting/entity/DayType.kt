ade52264dddb0200dfbd3e6410f7f3c4734adf4b
rkddlsgur983 <rkddlsgur983@naver.com>
2019-11-25 13:22:26 +0900

    refactor: #9 알람 설정 1단계 1. 세부 UI 조정 2. 클래스명 변경 SettingActivity1 -> SettingFirstActivity 3. 레포지토리 클래스 삭제 4. 클릭 이벤트 전달 (데이터바인딩 -> 뷰모델 -> 뷰) 5. 알람 설정 2단계로 전달하기 위한 모델 객체 선언 및 생성 (SettingFirstModel) 6. 요일, 시간 enum type으로 선언

---
+++ app/src/main/java/com/km/ticktock/views/alarmsetting/entity/DayType.kt
@@ -0,0 +1,11 @@
+package com.km.ticktock.views.alarmsetting.entity
+
+enum class DayType(val value: Int) {
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6),
+    SUNDAY(7)
+}
